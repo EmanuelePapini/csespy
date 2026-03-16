05405c0222bdc5f357d9a2781deae90373aebd59
Davide Giordano <davide.giordano@unitn.it>
2026-03-16 11:02:43 +0100

    improved example for reading zarr zip

--- examples/example_load_single_zarrzip.py
+++ examples/example_load_single_zarrzip.py
@@ -4,8 +4,7 @@ import csespy_dev as csespy
 
 # Absolute path to one .zarr.zip file
 zarr_file = (
-    "/storage/gpfs_data/limadou/data/cses_data/CSES01/EFD_ELF/2019/01/"
-    "CSES_01_SCM_2_L02_A2_053980_20190123_081535_20190123_082228_000.zarr.zip"
+    "/storage/gpfs_data/limadou/data/cses_data/CSES01/EFD_ELF/2020/01/CSES_01_EFD_2_L02_A1_110770_20200131_225632_20200131_233110_000.zarr.zip"
 )
 
 # Use unstructured_path=True so csespy searches directly in this folder
@@ -14,16 +13,10 @@ file_name = os.path.basename(zarr_file)
 
 css = csespy.CSES(path=data_dir, unstructured_path=True)
 
-# Select exactly this file by filename substring
 css.select_data_to_load(search_string=file_name, append=False)
+css.load_CSES(datakey="EFD_ELF", fix_data=False)
 
-# SCM instrument number 2 corresponds to ELF -> datakey is SCM_ELF
-css.load_CSES(datakey="SCM_ELF", fix_data=False)
-
-if "SCM_ELF" not in css.data:
-    raise RuntimeError("SCM_ELF was not loaded. Check path/datakey/search_string.")
-
-df = css.data["SCM_ELF"]
+df = css.data["EFD_ELF"]
 print("Loaded keys:", list(css.data.keys()))
-print("SCM_ELF shape:", df.shape)
+print("EFD_ELF shape:", df.shape)
 print(df.head())
